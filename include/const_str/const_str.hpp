bbfd5ccc5c97d373faef4ce99c08e28c74859dc3
Philip Wenkel <38218064+phiwen96@users.noreply.github.com>
2021-03-05 22:30:01 +0100

    a

---
+++ include/const_str/const_str.hpp
@@ -0,0 +1,85 @@
+#pragma once
+#include <iostream>
+
+template <int N>
+class const_str
+{
+    size_t m_size;
+    char m_str [N];
+    
+public:
+    
+    consteval const_str (char const(& str) [N]) : m_size (N), m_str()
+    {
+        std::copy (str, str + N, m_str);
+    }
+    
+    consteval auto size () const -> size_t
+    {
+        return m_size;
+    }
+    
+//    template <int M>
+//    constexpr auto operator== (const_str <M> const&) -> bool
+//    {
+//        return false;
+//    }
+    
+
+    constexpr auto operator[] (int i) const -> char
+    {
+        return m_str [i];
+    }
+    
+    template <size_t I, size_t J>
+    friend constexpr auto operator== (const_str <I> const& lhs, const_str <J> const& rhs) -> bool
+    {
+        return false;
+    }
+
+    friend constexpr auto operator== (const_str <N> const& lhs, const_str <N> const& rhs) -> bool
+    {
+        for (int i = 0; i < N; ++i)
+        {
+            if (lhs.m_str [i] != rhs.m_str[i])
+                return false;
+        }
+        return true;
+    }
+    
+    template <size_t I, size_t J>
+    friend constexpr auto operator== (const_str <I> const& lhs, char const(&rhs) [J]) -> bool
+    {
+        return false;
+    }
+
+    friend constexpr auto operator== (const_str <N> const& lhs, char const(rhs) [N]) -> bool
+    {
+        for (int i = 0; i < N; ++i)
+        {
+            if (lhs.m_str [i] != rhs[i])
+                return false;
+        }
+        return true;
+    }
+    
+    template <size_t M>
+    friend auto operator== (const_str <M> const& lhs, char const* rhs) -> bool
+    {
+        if (strlen(rhs) != M)
+            return false;
+        
+        for (int i = 0; i < M; ++i)
+        {
+            if (lhs.m_str [i] != rhs[i])
+                return false;
+        }
+        return true;
+    }
+    
+    friend auto operator<< (ostream& os, const_str const& s) -> ostream&
+    {
+        os << s.m_str;
+        return os;
+    }
+};
---
+++ tests/testlib.cpp
@@ -0,0 +1,19 @@
+#define CATCH_CONFIG_MAIN
+#include <catch2/catch.hpp>
+//#include <phulkan_generator/gpu.hpp>
+#include <const_str/const_str.hpp>
+
+
+using namespace std;
+
+
+consteval int kiss (){return 0;}
+
+TEST_CASE( "Quick check", "[kiss]" ) {
+//      cout << GPU_0_MAX_IMAGE_DIMENSION_1D << endl;
+//    cout << gpu<0>::features::fillModeNonSolid << endl;
+	std::cout << "hi" << std::endl;
+    REQUIRE( kiss () == 0 );
+}
+
+
