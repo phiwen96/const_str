cddd8a101e43de696c87c24d7816ba3c5222b66b
Philip Wenkel <38218064+phiwen96@users.noreply.github.com>
2021-03-26 16:36:09 +0100

    a

--- include/const_str/const_str.hpp
+++ include/const_str/const_str.hpp
@@ -135,3 +135,34 @@ constexpr char getch (char const (&s) [N], unsigned int i)
 
 #define _MACRO(z, n, text) BOOST_PP_COMMA_IF(n) getch (text, n)
 #define STR(s) decltype (_str<BOOST_PP_REPEAT (BOOST_PP_LIMIT_REPEAT, _MACRO, s)> {})
+
+
+
+/**
+namespace Color {
+    enum Code {
+        FG_RED      = 31,
+        FG_GREEN    = 32,
+        FG_BLUE     = 34,
+        FG_DEFAULT  = 39,
+        BG_RED      = 41,
+        BG_GREEN    = 42,
+        BG_BLUE     = 44,
+        BG_DEFAULT  = 49
+    };
+    class Modifier {
+        Code code;
+    public:
+        Modifier(Code pCode) : code(pCode) {}
+        friend std::ostream&
+        operator<<(std::ostream& os, const Modifier& mod) {
+            return os << "\033[" << mod.code << "m";
+        }
+    };
+}
+
+
+    Color::Modifier red(Color::FG_RED);
+    Color::Modifier def(Color::FG_DEFAULT);
+
+*/
